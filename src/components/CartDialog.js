72aba507cf5d4b62d3a5df19902fc4627c10d6c2
sreisner <shawn.reisner@gmail.com>
2018-09-23 08:21:04 -0500

    Add order subtotal to cart dialog

--- src/components/CartDialog.js
+++ src/components/CartDialog.js
@@ -9,6 +9,7 @@ import {
   Typography,
   IconButton,
   DialogTitle,
+  Divider,
 } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/CloseRounded';
 import DeleteIcon from '@material-ui/icons/DeleteOutlineRounded';
@@ -17,29 +18,33 @@ import RemoveIcon from '@material-ui/icons/RemoveCircleOutlineRounded';
 import EmptyCartIcon from '@material-ui/icons/RemoveShoppingCartRounded';
 import { ShoppingCartConsumer } from './ShoppingCartContext';
 import { getFormattedPrice } from '../utils';
+import classNames from 'classnames';
 
 const styles = theme => ({
+  row: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   dialogTitle: {
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  productRow: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+  productTitle: {
+    marginRight: theme.spacing.unit * 2,
   },
-  quantityModifier: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginLeft: theme.spacing.unit * 8,
+  orderSummary: {
+    marginBottom: theme.spacing.unit * 2,
   },
   productSubtotalContainer: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  divider: {
+    margin: `${theme.spacing.unit * 3}px ${-theme.spacing.unit * 3}px`,
   },
 });
 
@@ -63,26 +68,29 @@ let CartDialog = props => {
           open={cartDialogIsOpen}
           onClose={closeCartDialog}
         >
-          <DialogTitle>
-            <div className={classes.dialogTitle}>
-              <Typography variant="display2">Your Order</Typography>
+          <DialogContent>
+            <div className={classes.row}>
+              <Typography variant="display1">Your Order</Typography>
               <IconButton onClick={closeCartDialog}>
                 <CloseIcon fontSize="large" />
               </IconButton>
             </div>
-          </DialogTitle>
-          <DialogContent>
             {cart.length === 0 && (
               <Typography variant="display1">
                 <EmptyCartIcon /> Your cart is empty
               </Typography>
             )}
             {cart.map(cartProduct => (
-              <div className={classes.productRow} key={cartProduct.product._id}>
-                <Typography variant="headline">
+              <div className={classes.row} key={cartProduct.product._id}>
+                <Typography
+                  variant="subheading"
+                  className={classes.productTitle}
+                >
                   {cartProduct.product.title}
                 </Typography>
-                <div className={classes.quantityModifier}>
+                <div
+                  className={classNames(classes.row, classes.quantityModifier)}
+                >
                   {cartProduct.numItemsInCart === 1 && (
                     <IconButton
                       onClick={() => removeProductFromCart(cartProduct.product)}
@@ -115,6 +123,17 @@ let CartDialog = props => {
                 </div>
               </div>
             ))}
+            <Divider className={classes.divider} />
+            <Typography variant="display1" className={classes.orderSummary}>
+              Order Summary
+            </Typography>
+            <div className={classes.row}>
+              <Typography variant="subheading">Subtotal</Typography>
+              <Typography variant="title">
+                {getFormattedPrice(getCartSubtotal())}
+              </Typography>
+            </div>
+            <Divider className={classes.divider} />
           </DialogContent>
           <DialogActions>
             <Button
